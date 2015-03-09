7367f9cb17e22a6c463dfb73727074cc593aaeee
Jeff Berry <jeff@portonefive.com>
2015-03-09 14:34:21 -0600

    Remove example stuff, change to libsass foundation setup

--- src/LaraPress/Contracts/Posts/Repository.php
+++ src/LaraPress/Contracts/Posts/Repository.php
@@ -1,15 +1,6 @@
 <?php namespace LaraPress\Contracts\Posts;
 
-use App\Post;
-
 interface Repository {
-
-    /**
-     * @param $id
-     *
-     * @return Post
-     */
-    public function findById($id);
 
     /** @return string */
     public function getModelClassName();
